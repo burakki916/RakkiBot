d6db4498427780c2eff433e9c452469adb59a9cb
Makaio <makaio.hendrix@gmail.com>
2022-05-30 00:57:47 -0400

    now only I can restart the bot!

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { Client, Intents } = require('discord.js');
-const { token } = require('./token.json');
+const { token } = require('./devToken.json');
 const handler = require('./commandHandler');
 var calorant = require('./calorant');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
-  const channel = client.channels.cache.get("870558428995682314");
+  const channel = client.channels.cache.get("980694201220096010");
   channel.send("Bot is up!");
   calorant.init(client);
   client.user.setStatus("r!help for help"); 
@@ -18,7 +18,6 @@ client.once('ready', () => {
 });
 client.on('message',message =>{
 if(message.content.startsWith('r!')){
-    
     console.log("["+ message.author.username+"] " + message.content);
     handler.handle(message); 
     // if(message.content.startsWith("r!update")){
